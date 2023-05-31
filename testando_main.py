ba8638d06c6387a4e36d2b4eabaef838f3f21e9a
Lucas Gomes <lucasgomes.luc@gmail.com>
2023-05-31 20:07:47 -0300

    Testando a estrutura main() e fstrings

---
+++ testando_main.py
@@ -0,0 +1,10 @@
+def main():
+    name = "Marquinhos"
+    surname = "Maneiro"
+    age = 18
+
+    #Testando fstring
+    print(f"Sobrenome: {surname}, Nome: {name}, Idade: {age}")
+
+if __name__ == '__main__':
+    main()
