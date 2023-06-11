0d01d97e11e1fdf9854864db39f6734947e89fd8
xviigimenez <gimenez@tuta.io>
2023-06-11 17:29:19 -0300

    Consertado o problema 1161 do beecrowd para receber redirecionamento de entrada

--- beecrowd/1161/1161.py
+++ beecrowd/1161/1161.py
@@ -1,17 +1,21 @@
 # https://www.beecrowd.com.br/judge/pt/problems/view/1161
 
-input = input().split(" ")
+import fileinput
 
-if not int(input[0]) == 0:
-    for i in range(1, int(input[0])):
-        input[0] = int(input[0]) * i
-else:
-    input[0] = 1
+for line in fileinput.input():
 
-if not int(input[1]) == 0:
-    for i in range(1, int(input[1])):
-        input[1] = int(input[1]) * i
-else:
-    input[1] = 1
+    input = line.replace("\n", "").split(" ")
 
-print(int(input[0]) + int(input[1]))
+    if not int(input[0]) == 0:
+        for i in range(1, int(input[0])):
+            input[0] = int(input[0]) * i
+    else:
+        input[0] = 1
+
+    if not int(input[1]) == 0:
+        for i in range(1, int(input[1])):
+            input[1] = int(input[1]) * i
+    else:
+        input[1] = 1
+
+    print(int(input[0]) + int(input[1]))
